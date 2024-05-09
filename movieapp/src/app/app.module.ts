7e24d544af92a016961dc5e142bff31355ab2e06
Anastasia Moraites <anastasiamoraites@gmail.com>
2024-05-09 17:13:08 +0200

    added in a new component, buttons. Then started creating the toolbar and added html and css coding

--- movieapp/src/app/app.module.ts
+++ movieapp/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
 import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
 import { AddMoviesComponent } from './components/add-movies/add-movies.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolBarComponent,
     MoviesHomeComponent,
-    AddMoviesComponent
+    AddMoviesComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
